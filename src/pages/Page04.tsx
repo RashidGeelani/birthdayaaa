91f148326c6c3ceff0b99b8eacdb496ae436e6b3
rashid geelani <syed.geelani@vit.edu.in>
2026-08-09 01:33:23 +0530

    changes

--- src/pages/Page04.tsx
+++ src/pages/Page04.tsx
@@ -1,10 +1,7 @@
 import photo1 from "../assets/photo1.jpeg";
-import photo2 from "../assets/photo2.jpeg";
 import photo3 from "../assets/photo3.jpeg";
-import photo4 from "../assets/photo4.jpeg";
 import photo5 from "../assets/photo5.jpeg";
 import photo6 from "../assets/photo6.jpeg";
-import photo7 from "../assets/photo7.jpeg";
 import photo8 from "../assets/photo8.jpeg";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
